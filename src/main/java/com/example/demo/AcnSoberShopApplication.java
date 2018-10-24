6a3bd64853e5474925eb6ca8b688abbb677750e3
Girolamo <girolamo9129@gmail.com>
2018-10-24 15:02:16 +0200

    New Project

---
+++ src/main/java/com/example/demo/AcnSoberShopApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcnSoberShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcnSoberShopApplication.class, args);
+	}
+}
