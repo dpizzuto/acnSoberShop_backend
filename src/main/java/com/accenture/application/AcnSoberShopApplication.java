81a415012e978b05d3022074b1d2648628e52121
Girolamo <girolamo9129@gmail.com>
2018-10-24 15:34:24 +0200

    development

--- src/main/java/com/accenture/application/AcnSoberShopApplication.java
+++ src/main/java/com/accenture/application/AcnSoberShopApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.accenture.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
