0308017ae8e753381b1d1a317d521469ab0b771e
Daniele Radicia <danieleradiciat@gmail.com>
2018-10-25 19:09:26 +0200

    Alcune modifiche per vedere se mi fa pushare

--- src/main/java/com/accenture/controller/UserController.java
+++ src/main/java/com/accenture/controller/UserController.java
@@ -1,5 +1,23 @@
 package com.accenture.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.accenture.model.*;
+
+@RestController
 public class UserController {
+
+	@RequestMapping("/userAcn")
+	public User adduser(@RequestParam(value = "name") String name, @RequestParam(value = "surname") String surname,
+			@RequestParam(value = "gender") String gender, @RequestParam(value = "age") int age,
+			@RequestParam(value = "email") String email, @RequestParam(value = "password") String password)
+
+	{
+		User user = new User(name, surname, gender, age, email, password);
+		return user;
+
+	}
 
 }
--- src/main/java/com/accenture/model/User.java
+++ src/main/java/com/accenture/model/User.java
@@ -1,5 +1,75 @@
 package com.accenture.model;
 
 public class User {
+	public String name;
+	public String surname;
+	public String gender;
+	public int age;
+	public String email;
+	public String password;
+
+	public User(String name, String surname, String gender, int age, String email, String password) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.gender = gender;
+		this.age = age;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", surname=" + surname + ", gender=" + gender + ", age=" + age + ", Email="
+				+ email + ", password=" + password + "]";
+	}
 
 }
